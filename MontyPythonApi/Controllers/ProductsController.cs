08376cb6be7c6be59e8d861d2a36bd507cb8797e
Hilary Weaver-Robb <hilaryweaver-robb@quickenloans.com>
2017-01-19 23:37:03 -0500

    cleaned up some extra folders (not sure how it got that way), and fixed a typo so index.html works

--- MontyPythonApi/Controllers/ProductsController.cs
+++ MontyPythonApi/Controllers/ProductsController.cs
@@ -12,9 +12,9 @@ namespace MontyPythonApi.Controllers
 	{ //https://www.asp.net/web-api/overview/getting-started-with-aspnet-web-api/tutorial-your-first-web-api
 		Product[] products = new Product[]
 		{
-new Product {Id = 1, Name = "Rabid Bunny Slippers", Category = "Apparel", Price = 39.95M },
-new Product {Id = 2, Name = "Black Knight Bobblehead", Category = "Toys", Price = 9.95M },
-new Product {Id = 3, Name = "Tim the Enchanter Stocking", Category = "Holiday", Price = 32.50M }
+			new Product {Id = 1, Name = "Rabid Bunny Slippers", Category = "Apparel", Price = 39.95M },
+			new Product {Id = 2, Name = "Black Knight Bobblehead", Category = "Toys", Price = 9.95M },
+			new Product {Id = 3, Name = "Tim the Enchanter Stocking", Category = "Holiday", Price = 32.50M }
 		};
 
 		public IEnumerable<Product> GetAllProducts()
--- MontyPythonApi/MontyPythonApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MontyPythonApi.WebApiApplication" Language="C#" %>
